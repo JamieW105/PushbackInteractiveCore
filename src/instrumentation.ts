257d45bc54a1fdb2517fef60ff1c188e3c15cb8b
JamieW105 <jamie.wells@stpats.school.nz>
2026-01-15 22:36:12 +1300

    Fixes

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,7 +2,8 @@
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
         const { getQotdBot, getMgmtBot } = await import('@/lib/bot')
-        getQotdBot()
-        getMgmtBot()
+        await getQotdBot()
+        await getMgmtBot()
+        console.log('[INSTRUMENTATION] Discord bots initialized')
     }
 }
